cfed1b3a0e2a959266c6390d295abbcc35d1bc49
yuhkalhic <146904269+yuhkalhic@users.noreply.github.com>
2025-02-02 12:47:53 +0800

    Update reader.py

--- models/reader.py
+++ models/reader.py
@@ -1,14 +1,14 @@
 from utils.extract_option import extract_option
 
 
-cot_template = """You are a professional medical expert to answer the # Question. Please first think step-by-step and then answer the question. Your responses will be used for research purposes only, so please have a definite answer.
-You should think step by step and respond in the format <answer>A/B/C/D</answer> (only one option can be chosen) at the end of your response. Please keep your entire response simple and complete, up to 100 words.
+cot_template = """You are a professional medical expert to answer the # Question. Please first using the # Retrieved Documents and then answer the question. Your responses will be used for research purposes only, so please have a definite and direct answer.
+You should respond in the format <answer>A/B/C/D</answer> (only one option can be chosen) at your response.
 
 # Question
 {question}"""
 
-ret_template = """You are a professional medical expert to answer the # Question. Please first think step-by-step using the # Retrieved Documents and then answer the question. Your responses will be used for research purposes only, so please have a definite answer.
-You should think step by step and respond in the format <answer>A/B/C/D</answer> (only one option can be chosen) at the end of your response. Please keep your entire response simple and complete, up to 100 words.
+ret_template = """You are a professional medical expert to answer the # Question. Please first using the # Retrieved Documents and then answer the question. Your responses will be used for research purposes only, so please have a definite and direct answer.
+You should respond in the format <answer>A/B/C/D</answer> (only one option can be chosen) at your response.
 
 # Retrieved Documents
 {documents}
